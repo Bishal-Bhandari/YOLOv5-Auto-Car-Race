6550bc9aa0ed87ca5684178bca14b3d5169cf5f3
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-06-26 10:23:41 +0200

    maintain the code

--- main.py
+++ main.py
@@ -54,6 +54,14 @@ def details():
     screen.blit(text_3, (50, 250))
 
 
+def display_set():
+    # setting up the display
+    pygame.draw.rect(screen, (50, 50, 50), (width / 2 - road_w / 2, 0, road_w, height))
+    pygame.draw.rect(screen, (255, 240, 60), (width / 2 - roadmark_w / 2, 0, roadmark_w, height))
+    pygame.draw.rect(screen, (255, 255, 255), (width / 2 - road_w / 2 + roadmark_w * 1, 0, roadmark_w, height))
+    pygame.draw.rect(screen, (255, 255, 255), (width / 2 + road_w / 2 - roadmark_w * 2, 0, roadmark_w, height))
+
+
 car2 = pygame.image.load("asset/police.png")
 car2_loc = car2.get_rect()
 car2_loc.center = left_lane, height * 0.2
@@ -80,11 +88,6 @@ while running:
         else:
             car2_loc.center = left_lane, -200
 
-    # for random car
-    a = 2
-    a *= 1.5
-    randcar = car2_loc[1] + a
-
     # collapse
     if car_loc[0] == car2_loc[0] and car2_loc[1] > car_loc[1] - 250:
         print("Game Over!!!")
@@ -100,17 +103,16 @@ while running:
             if event.key in [K_d, K_RIGHT]:
                 car_loc = car_loc.move([int(road_w / 2), 0])
 
-    # setting up the display
-    pygame.draw.rect(screen, (50, 50, 50), (width / 2 - road_w / 2, 0, road_w, height))
-    pygame.draw.rect(screen, (255, 240, 60), (width / 2 - roadmark_w / 2, 0, roadmark_w, height))
-    pygame.draw.rect(screen, (255, 255, 255), (width / 2 - road_w / 2 + roadmark_w * 1, 0, roadmark_w, height))
-    pygame.draw.rect(screen, (255, 255, 255), (width / 2 + road_w / 2 - roadmark_w * 2, 0, roadmark_w, height))
+    display_set()
 
     # display block
     screen.blit(car, car_loc)
     screen.blit(car2, car2_loc)
 
     # for random car
+    a = 2
+    a *= 1.5
+    randcar = car2_loc[1] + a
     if randcar > height:
         opp_vec1 = car2load()
         car2 = opp_vec1
